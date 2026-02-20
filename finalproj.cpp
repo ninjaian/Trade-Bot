279a198f489080cd1d1a1cedccaec29af440a4ea
Ian <oofroblox6969@gmail.com>
2026-02-19 17:43:19 -0800

    buy/sell/trade/userbalance/userinventory functions added

--- finalproj.cpp
+++ finalproj.cpp
@@ -37,6 +37,8 @@ void printArray(Knife knives[], int size); //prints array nicely
 void buyInfo(); //info to teach user to buy
 void sellInfo(); //info to teach user to sell
 void tradeInfo(); //info to teach user to trade
+void printBalance(const UserData ud);
+void printInventory(const UserData ud);
 
 //CSV FILE STUFF
 bool openFile(ifstream& ifile, char ifileName[]); //attempts to open csv file
@@ -44,19 +46,19 @@ void populateStoreArray(ifstream& ifile, Knife knives[], int& size); //populated
 void populateMegaArray(ifstream& datafile, AllKnife& allKnives); //populate nested struct with mega data
 
 //USER INPUT
-void readMenu(char& userChar, Knife knives[], int& size, bool& quit, UserData& ud); //very important function that reads menu input and then executes their command
-void buy(Knife knives[], UserData& ud); //takes knives store front list and user data (removing from knife list)
-void sell(Knife knives[], UserData& ud); //takes knives store front list and user data (adds to knife list and user balance)
-void trade(Knife knives[], UserData& ud); //takes knives store front list and user data (swaps user knife for store front knife and adds balance)
+void readMenu(char& userChar, Knife knives[], int& size, bool& quit, UserData& ud, char userType[], char userSkin[], AllKnife& allKnives); //very important function that reads menu input and then executes their command
+void buy(Knife knives[], UserData& ud, int& size, char userType[], char userSkin[], AllKnife& allKnives); //takes knives store front list and user data (removing from knife list)
+void sell(Knife knives[], UserData& ud, int& size, char userType[], char userSkin[], AllKnife& allKnives); //takes knives store front list and user data (adds to knife list and user balance)
+void trade(Knife knives[], UserData& ud, int& size, char userType[], char userSkin[], AllKnife& allKnives); //takes knives store front list and user data (swaps user knife for store front knife and adds balance)
 
 //USER INPUT VALIDITY CHECKS
 bool charValid(char userChar); //checks that the char is valid true if valid
-bool readType(AllKnife& allKnives, char userType[]); //read knife type from user and verifies it is valid
-bool readSkin(AllKnife& allKnives, char userSkin[]); //read skin type from user and verifies it is valid
-
+bool readType(AllKnife& allKnives, char userType[], int& typeIndex); //read knife type from user and verifies it is valid
+bool readSkin(AllKnife& allKnives, char userSkin[], int& skinIndex, int typeIndex); //read skin type from user and verifies it is valid
+void validKnife(AllKnife& allKnives, char userType[], char userSkin[], int& typeIndex, int& skinIndex, int& index);
 // INSERT AND REMOVE FROM STOREFRONT AND USER INVENTORY
-void insertStore(Knife knives[], int size);
-void removeStore(Knife knives[], int size);
+void insertStore(Knife knives[], int& size, AllKnife& allKnives, int index, UserData& ud);
+void removeStore(Knife knives[], int& size, int targetIndex);
 void insertInventory(UserData& ud);
 void removeInventory(UserData& ud);
 
@@ -100,39 +102,148 @@ int main(){
   welcome();
 
   while(!quit){
-    readMenu(userChar, knives, size, quit, ud);
+    readMenu(userChar, knives, size, quit, ud, userType, userSkin, allKnives);
+    
   }
   
   ifile.close();
 }
-void buy(Knife knives[], UserData& ud){
+void buy(Knife knives[], UserData& ud, int& size, char userType[], char userSkin[], AllKnife& allKnives){
   buyInfo();
-  //create function for user to buy knife with balance
+  int typeIndex = 0;
+  int skinIndex = 0;
+  int index = 0;
+  int targetIndex = 0;
+  bool found = false;
+  validKnife(allKnives, userType, userSkin, typeIndex, skinIndex, index); 
+  for(int i = 0; i<size; i++){
+    if(strcmp(knives[i].type, userType) == 0 && strcmp(knives[i].skin, userSkin) == 0){
+      found = true;
+      targetIndex = i;
+      cout << endl << endl;
+      //cout << "Located knife in storefront." << endl;
+      cout << knives[i].type << " " << knives[i].skin << " $" << knives[i].price << endl;
+      if(ud.balance >= knives[i].price){ 
+        ud.balance -= knives[i].price;
+        ud.userInventory[ud.knifeCount] = knives[i];
+        cout << ud.userInventory[ud.knifeCount].type << " " << ud.userInventory[ud.knifeCount].skin << " in your inventory" << endl; 
+        ud.knifeCount++;
+        cout << "Your remaining balance is: $" << ud.balance << endl;
+        removeStore(knives, size, targetIndex);
+      } else {
+        cout << "Insufficient balance! You have $" << ud.balance << " in balance." << endl; 
+      }
+    }
+  }
+  if(!found){
+    cout << " Could not locate knife in storefront..." << endl;
+  }
 }
-void sell(Knife knives[], UserData& ud){
+void sell(Knife knives[], UserData& ud, int& size, char userType[], char userSkin[], AllKnife& allKnives){
   sellInfo();
-  //create function for user to sell knife for balance
+  int skinIndex = 0;
+  int typeIndex = 0;
+  int index = 0;
+  validKnife(allKnives, userType, userSkin, typeIndex, skinIndex, index);
+  insertStore(knives, size, allKnives, index, ud);
+  ud.balance = allKnives.knives[index].price;
 }
-void trade(Knife knives[], UserData& ud){
+void trade(Knife knives[], UserData& ud, int& size, char userType[], char userSkin[], AllKnife& allKnives){
   tradeInfo();
-  //create function for user to trade knife and get left over balance
+  int skinIndex = 0;
+  int typeIndex = 0;
+  int index = 0;
+  char userTypeTrade[MAXCHAR] = {0};
+  char userSkinTrade[MAXCHAR] = {0};
+  bool found = false;
+  int targetIndex = 0;
+  cout << "Enter the type of knife from the storefront : ";
+  cin.getline(userTypeTrade, MAXCHAR);
+  cout << "Enter the skin of the knife from the storefront : ";
+  cin.getline(userSkinTrade, MAXCHAR);
+  for(int i = 0; i<size; i++){
+    if(strcmp(knives[i].type, userTypeTrade) == 0 && strcmp(knives[i].skin, userSkinTrade) == 0){
+      cout << endl << endl;
+      cout << "Located knife in storefront." << endl;
+      cout << knives[i].type << " " << knives[i].skin << " $" << knives[i].price << endl;
+      found = true;
+    }
+    if(found){
+      targetIndex = i;
+      cout << endl <<"Please enter the name of the knife you wish to trade for it." << endl;
+      validKnife(allKnives, userType, userSkin, typeIndex, skinIndex, index);
+      if(allKnives.knives[index].price >= knives[i].price){
+        ud.balance += (allKnives.knives[index].price - knives[i].price);
+        ud.userInventory[ud.knifeCount] = knives[i];
+        cout << ud.userInventory[ud.knifeCount].type << " " << ud.userInventory[ud.knifeCount].skin << " in your inventory" << endl;  
+        ud.knifeCount++;
+        insertStore(knives, size, allKnives, index, ud);
+        removeStore(knives, size, targetIndex);
+      }
+      i = size;
+    }
+  }
+  if(!found){
+    cout << "Could not find knife in storefront!" << endl;
+  }
 }
-bool readSkin(AllKnife& allKnives, char userSkin[]){
-  for(int i = 0; i>allKnives.len; i++){
+void insertStore(Knife knives[], int& size, AllKnife& allKnives, int index, UserData& ud){
+  if(size+1 < DATACAP){
+    strcpy(knives[size].type, allKnives.knives[index].type); //NEED TO FIX THIS
+    strcpy(knives[size].skin, allKnives.knives[index].skin);
+    knives[size].price = allKnives.knives[index].price;
+    cout << "Adding " << knives[size].type << " | " << knives[size].skin << " To the store" << endl;
+    //ud.balance += knives[size].price;
+    //cout << "Added $" << knives[size].price << " to your balance. Your current balance is now $" << ud.balance << endl;
+    size++;
+  }
+}
+void removeStore(Knife knives[], int& size, int targetIndex){
+  for(int i = targetIndex; i<size; i++){
+    knives[i] = knives[i+1];
+  }
+  size--;
+}
+void validKnife(AllKnife& allKnives, char userType[], char userSkin[], int& typeIndex, int& skinIndex, int& index){
+  bool valid = false;
+  while(!valid){ //make sure valid knife
+    cin.clear();
+    cout << "Enter the knife type : ";
+    cin.getline(userType, MAXCHAR);
+    cout << "Enter the knife skin : ";
+    cin.getline(userSkin, MAXCHAR);
+    cout << endl;
+    if(readType(allKnives, userType, typeIndex)){
+        if(readSkin(allKnives, userSkin, skinIndex, typeIndex)){
+          index = skinIndex;
+          cout << userSkin << " " << userType << " is a valid knife. Price: $" << allKnives.knives[index].price << endl;
+          valid = true;
+        }
+    }else {
+      cout << "...Invalid Knife" << endl;
+    }
+  }
+}
+bool readSkin(AllKnife& allKnives, char userSkin[], int& skinIndex, int typeIndex){
+  for(int i = typeIndex -1; i<allKnives.len; i++){
     if(strcmp(allKnives.knives[i].skin, userSkin) == 0){
+      skinIndex = i;
       return true;
     }
   }
   return false;
 }
-bool readType(AllKnife& allKnives, char userType[]){
+
+bool readType(AllKnife& allKnives, char userType[], int& typeIndex){
   for(int i = 0; i<allKnives.len; i++){
     if(strcmp(allKnives.knives[i].type, userType) == 0){
+      typeIndex = i;
       return true;
     }
   }
   return false;
 }
+
 void populateMegaArray(ifstream& datafile, AllKnife& allKnives){
   datafile.ignore(1024, '\n');
   while(!datafile.eof() && allKnives.len < 431){
@@ -148,9 +259,11 @@ void populateMegaArray(ifstream& datafile, AllKnife& allKnives){
   allKnives.len--;
 }
 
-void readMenu(char& userChar, Knife knives[], int& size, bool& quit, UserData& ud){
+void readMenu(char& userChar, Knife knives[], int& size, bool& quit, UserData& ud, char userType[], char userSkin[], AllKnife& allKnives){
   displayMenu();
   cin >> userChar;
+  cin.clear();
+  cin.ignore(100, '\n');
   while(!charValid(userChar)){
     cout << "Please enter a valid character!" << endl;
     displayMenu();
@@ -166,23 +279,23 @@ void readMenu(char& userChar, Knife knives[], int& size, bool& quit, UserData& u
   }
   if (userChar == 't' || userChar == 'T'){ //TRADE
     printArray(knives, size);
-    trade(knives, ud);
+    trade(knives, ud, size, userType, userSkin, allKnives);
   }
   if (userChar == 'b' || userChar == 'B'){ //BUY
     printArray(knives, size);
-    buy(knives, ud);
+    buy(knives, ud, size, userType, userSkin, allKnives);
   }
   if (userChar == 's' || userChar == 'S'){ //SELL
-    sell(knives, ud);
+    sell(knives, ud, size, userType, userSkin, allKnives);
   }
   if (userChar == 'p' || userChar == 'P'){ //PRINT KNIVES
     printArray(knives, size);
   }
   if (userChar == 'w' || userChar == 'W'){ //print wallet
-    //printBal(balance);
+    printBalance(ud);
   }
   if (userChar == 'i' || userChar == 'I'){ //print user inventory
-    //printInventory();
+    printInventory(ud);
   }
 }
 bool charValid(char userChar){ //NEED TO ADD ONE TO CHECK BALANCE and INVENTORY (Wallet, Inventory)
@@ -211,7 +324,7 @@ void populateStoreArray(ifstream& ifile, Knife knives[], int& size){
     }
   }
   if(endFound){
-    size--;
+    //size--; why is this breaking
   }
 }
 void printArray(Knife knives[], int size){
@@ -223,6 +336,7 @@ void printArray(Knife knives[], int size){
     for(int i = 0; i<size; i++){
       cout << left<< setw(30) << knives[i].type << setw(30) << knives[i].skin << setw(15) << knives[i].price << endl;
     }
+    cout << "----------------------------------------------------------------------------------------------" << endl;
 }
 bool openFile(ifstream& ifile, char ifileName[]){
   ifile.open(ifileName);
@@ -252,21 +366,43 @@ void displayMenu(){
 }
 void buyInfo(){
   cout << endl;
-  cout << "Please type the name of the knife you would like to buy from the storefront!" << endl;
-  cout << "ex. Karambit Fade" << endl;
-  cout << "If you have sufficient balance to buy it then the knife will transfer to your inventory." << endl;
+  cout << "Please enter the type and skin of the knife you would like to buy from the storefront! You must have balance to use this feature!" << endl;
+  cout << "ex." << endl;
+  cout << "Karambit" << endl;
+  cout << "Fade" << endl;
+  cout << "If you have sufficient balance to buy it then the knife will transfer to your inventory." << endl << endl;
 }
 void sellInfo(){
   cout << endl;
-  cout << "Please type the name of the knife you would like to sell to us!" << endl;
-  cout << "ex. Butterfly Vanilla" << endl;
-  cout << "If the knife is valid then we will send you balance for it which you can then use to buy something else." << endl;
+  cout << "Please enter the type and skin of the knife you would like to sell to us!" << endl;
+  cout << "ex." << endl;
+  cout << "Butterfly" << endl;
+  cout << "Vanilla" << endl;
+  cout << "If the knife is valid then we will send you balance for it which you can then use to buy something else." << endl << endl;
 }
 void tradeInfo(){
   cout << endl;
-  cout << "Please type the name of the knife you would like to trade for from the storefront" << endl;
-  cout << "ex. Talon Doppler" << endl;
-  cout << "You will then be prompted to type the name of the knife you want to trade for it" << endl;
-  cout << "ex. Flip Tiger Tooth" << endl;
-  cout << "If the price of your knife is equivalent or greater, the trade will go through and you will get the left over balance" << endl;
+  cout << "Please enter the type and skin of the knife you want to trade for from the storefront" << endl;
+  cout << "ex." << endl;
+  cout << "Talon Doppler" << endl;
+  cout << "You will then be prompted to enter the type and skin of the knife you would like to trade for it" << endl;
+  cout << "ex." << endl;
+  cout << "Flip" << endl;
+  cout << "Tiger Tooth" << endl;
+  cout << "If the price of your knife is equivalent or greater, the trade will go through and you will get the left over balance" << endl << endl;
+}
+void printBalance(const UserData ud){
+  cout << endl;
+  cout << "Your current balance is: $" << ud.balance << endl;
+}
+void printInventory(const UserData ud){
+  cout << endl;
+  if(ud.knifeCount > 0){
+    cout << "Knives in inventory: " << endl;
+    for(int i = 0; i<ud.knifeCount; i++){
+      cout << ud.userInventory[i].type << " | " << ud.userInventory[i].skin << " | $" << ud.userInventory[i].price << endl;
+    }
+  } else {
+    cout << "Your inventory is empty!" << endl;
+  }
 }
